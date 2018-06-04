66e369fbba96ad07a9442f4dd9eacfe0af1d2ef2
KK <krishudr@gmail.com>
2018-06-04 16:22:53 +0530

    01 changes

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from Trunk");
+            Console.WriteLine("Hello World from Trunk - 01");
         }
     }
 }
